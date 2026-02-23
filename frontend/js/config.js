92a8c635b6f4b7d4b1f1565b48b6bd8767f281df
JawadAhmadCS <mjawadahmad41@gmail.com>
2026-02-24 00:03:03 +0500

    vps

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,2 @@
-// window.API_BASE_URL = "http://72.62.35.115:5000";
-window.API_BASE_URL = "http://127.0.0.1:5000";
+window.API_BASE_URL = "http://72.62.35.115:5000";
+// window.API_BASE_URL = "http://127.0.0.1:5000";
